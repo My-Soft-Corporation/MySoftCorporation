e6378413296c1cc7cc5d58a77106cb809abfd84f
Ahmad Ali <52967349+MySoft-Corporation@users.noreply.github.com>
2021-04-08 09:52:14 +0500

    Web Api host removed

--- Global.asax.cs
+++ Global.asax.cs
@@ -14,7 +14,6 @@ namespace MySoftCorporation
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
