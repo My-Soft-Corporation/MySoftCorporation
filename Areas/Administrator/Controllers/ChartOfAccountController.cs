271d0821643ef2152f517c1c27f251ed0c309528
Ahmad Ali <52967349+MySoft-Corporation@users.noreply.github.com>
2021-04-09 17:47:48 +0500

    Moduel : ChartOfAccounts : Integerated

---
+++ Areas/Administrator/Controllers/ChartOfAccountController.cs
@@ -0,0 +1,61 @@
+﻿using MySoft.Accounts.Entities.Models.Accounts;
+using MySoftCorporation.Helpers;
+using MySoftCorporation.Services.Services.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySoftCorporation.Areas.Administrator.Controllers
+{
+    public class ChartOfAccountController : Controller
+    {
+        private readonly ChartOfAccountService _service;
+        private readonly AccountTypeService accountTypeService;
+        public ChartOfAccountController()
+        {
+            _service = new ChartOfAccountService();
+            accountTypeService = new AccountTypeService();
+        }
+        public async Task<ActionResult> Index()
+        {
+
+            return View(await _service.GetAccountTypesAsync());
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult> Create()
+        {
+            ChartOfAccount accountType = new ChartOfAccount()
+            {
+                IP = UserInfo.IP(),
+                Agent = UserInfo.Agent(),
+                UserID = UserHelperInfo.GetUserId()
+            };
+            ViewBag.AccountTypesList = await accountTypeService.GetAccountTypesAsync();
+            return View(accountType);
+        }
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ChartOfAccount accountType)
+        {
+            if (ModelState.IsValid)
+            {
+                var (IsTrue, ResponseMsg) = await _service.Save(accountType);
+                if (IsTrue && ResponseMsg == "Success")
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("Unique", ResponseMsg);
+                    return View(accountType);
+                }
+            }
+            return View(accountType);
+        }
+    }
+}
---
+++ MySoftCorporation.Services/Services/Account/ChartOfAccountService.cs
@@ -0,0 +1,46 @@
+﻿using MySoft.Accounts.Entities.Models.Accounts;
+using MySoft.ErrorLoging.Entities;
+using MySoftCorporation.Data.Entities;
+using MySoftCorporation.Services.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySoftCorporation.Services.Services.Account
+{
+   public class ChartOfAccountService
+    {
+        private readonly MySoftCorporationDbContext _context;
+        public ChartOfAccountService()
+        {
+            _context = new MySoftCorporationDbContext();
+        }
+        public async Task<IEnumerable<ChartOfAccount>> GetAccountTypesAsync()
+        {
+            return await _context.ChartOfAccounts.Include(x => x.User).Include(x=>x.AccountType).ToListAsync();
+        }
+        public async Task<(bool IsTrue, string ResponseMsg)> Save(ChartOfAccount accountType)
+        {
+            if (accountType.Id > 0)
+            {
+                _context.Entry(accountType).State = EntityState.Modified;
+            }
+            else
+            {
+                _context.ChartOfAccounts.Add(accountType);
+            }
+            try
+            {
+                return (await _context.SaveChangesAsync() > 0, Result.Success);
+            }
+            catch (Exception exc)
+            {
+                return (false, Error.GetDetail(exc));
+
+            }
+        }
+    }
+}
